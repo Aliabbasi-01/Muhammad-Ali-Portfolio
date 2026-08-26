1570b53cf8ab3888736dcd09bb6929cf5177c5f7
Muhammad Ali <104286224+Aliabbasi-01@users.noreply.github.com>
2026-08-26 18:55:53 +0500

    rightbanner

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -4,20 +4,6 @@ import { Profile } from "../../assets/index";
 const RightBanner = () => {
   return (
     <div className="w-1/2 flex justify-center items-start relative">
-
-      {/* Background Card */}
-      <div
-        className="
-          absolute top-8
-          w-[140px] h-[180px]
-          sm:w-[200px] sm:h-[240px]
-          lgl:w-[300px] lgl:h-[300px]
-          bg-gradient-to-r from-[#1e2024] to-[#202327]
-          shadow-shadowOne rounded-lg
-        "
-      ></div>
-
-      {/* Profile Image */}
       <img
         className="
           relative z-10
@@ -29,7 +15,6 @@ const RightBanner = () => {
         src={Profile}
         alt="Muhammad Ali"
       />
-
     </div>
   );
 };
